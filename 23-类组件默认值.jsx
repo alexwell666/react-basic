158dffda361a786eed44acc3eb1a851c806ed8b2
alexwell666 <wrc2021072601@gmail.com>
2023-01-15 14:11:48 +0800

    类组件的默认值

--- 23-类组件默认值.jsx
+++ 23-类组件默认值.jsx
@@ -2,33 +2,26 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
-
 class Test extends React.Component {
-
   static defaultProps = {
-    pageSize: 10
+    pageSize: 10,
   }
-  render () {
-    return (
-      <div>{this.props.pageSize}</div>
-    )
+  render() {
+    return <div>{this.props.pageSize}</div>
   }
 }
 // Test.defaultProps = {
 //   pageSize: 10
 // }
 
-
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <div>
-        <Test ></Test>
-
+        <Test></Test>
       </div>
     )
   }
 }
-
 
 export default App
