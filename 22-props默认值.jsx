3e291ed4bc16bafda3772d69509682371324ce88
alexwell666 <wrc2021072601@gmail.com>
2023-01-15 14:01:04 +0800

    props default

--- 22-props默认值.jsx
+++ 22-props默认值.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
-
-
 //推荐写法直接这里pageSize=10(默认值)
-function Test ({ list, pageSize }) {
+function Test({ list, pageSize }) {
   return (
     <>
-
-      <div>{list.map(item => <p>{item}</p>)}</div>
+      <div>
+        {list.map((item) => (
+          <p>{item}</p>
+        ))}
+      </div>
       <div>{pageSize}</div>
     </>
   )
 }
 
-
 Test.propTypes = {
-  list: PropTypes.array.isRequired
+  list: PropTypes.array.isRequired,
 }
 //另外一种写法
 // Test.defaultProps = {
@@ -25,20 +25,13 @@ Test.propTypes = {
 // }
 
 class App extends React.Component {
-
-
-
-  render () {
-
-
+  render() {
     return (
       <div>
         <Test list={[1, 2, 3]} pageSize={30}></Test>
-
       </div>
     )
   }
 }
-
 
 export default App
