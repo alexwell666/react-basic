ed85f43a73b2b8ae0408aa16754cf6e56793ce1f
alexwell666 <wrc2021072601@gmail.com>
2023-01-15 13:54:06 +0800

    props校验

--- 21-props校验.jsx
+++ 21-props校验.jsx
@@ -1,34 +1,32 @@
 import React from 'react'
 
+//这里Pro大写
 import PropTypes from 'prop-types'
 
-
-function Test ({ list }) {
+function Test({ list }) {
   return (
-    <div>{list.map(item => <p>{item}</p>)}</div>
+    <div>
+      {list.map((item) => (
+        <p>{item}</p>
+      ))}
+    </div>
   )
 }
 
-
+//这里小写
 Test.propTypes = {
-  list: PropTypes.array.isRequired
+  //这里大写
+  list: PropTypes.array.isRequired,
 }
 
 class App extends React.Component {
-
-
-
-  render () {
-
-
+  render() {
     return (
       <div>
         <Test list={[1, 2, 3]}></Test>
-
       </div>
     )
   }
 }
-
 
 export default App
