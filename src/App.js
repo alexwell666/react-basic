ec25d8c6bfe948ab4f108f0c6985b9294f07edd1
wangrongchao <wangrongchao@jingxin.com>
2023-01-15 13:37:56 +0800

    PropTypes校验

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React from 'react'
 
-//test
+import PropTypes from 'prop-types'
 
-function ListItem ({ item, delItem, children }) {
+
+function Test ({ list }) {
   return (
-    <div>
-      <>
-        <p>{item.name}</p>
-        <p>{item.price}</p>
-        <p>children</p>
-        <button onClick={() => delItem(item.id)}>del</button>
-      </>
-    </div>
+    <div>{list.map(item => <p>{item}</p>)}</div>
   )
 }
 
 
-
+Test.PropTypes = {
+  list: PropTypes.array
+}
 
 class App extends React.Component {
 
-  state = ({
-    list: [
-      { id: 1, name: 'this is 1', price: 11 },
-      { id: 2, name: 'this is 2', price: 12 },
-      { id: 3, name: 'this is 3', price: 13 }
-    ]
-  })
-
-  delItem = (id) => {
-    console.log(id)
-    this.setState({
-      list: this.state.list.filter((item) => item.id !== id)
-    })
-  }
 
 
   render () {
@@ -41,8 +23,8 @@ class App extends React.Component {
 
     return (
       <div>
-        {this.state.list.map(item => (
-          <ListItem item={item} delItem={this.delItem}>this is children</ListItem>))}
+        <Test list={[1, 2]}></Test>
+
       </div>
     )
   }
